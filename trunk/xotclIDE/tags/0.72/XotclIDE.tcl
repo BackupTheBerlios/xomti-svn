27c521f1e4608fb84001ceee0cb6572fe51fa0bf
fabrice_pardo <fabrice_pardo@597427b9-74ee-0310-9ab7-f5dcdbef3a4c>
2005-01-25 21:00:07 +0000

    import of xotclIDE-0.72

---
+++ trunk/xotclIDE/tags/0.72/XotclIDE.tcl
@@ -0,0 +1,26 @@
+#!/usr/local/bin/xowish
+# start File for use XotclIDE as stand alone application
+# please adapt the xotcl library path for your machine
+# you can use tk or tix for XotclIDE
+# (you can not use TixInspector without tix)
+
+set sname [info script]
+if {$sname==""} {
+    # Run interactive for develop purposes
+    set xotclidedir /home/artur/programs/xotclIDE
+} else {
+    file lstat $sname stats
+    # follow sym links
+    if {$stats(type)=="link"} {
+	set sname [file readlink $sname]
+	if {[file pathtype $sname]=="relative"} {
+	    set sname [file join [file dirname [info script]] $sname]
+	}
+    }
+    set xotclidedir [file dirname $sname]
+    set xotclidedir [file normalize $xotclidedir]
+}
+source [file join $xotclidedir ideBase.tcl]
+
+IDEStarter startIDEOnly
+
