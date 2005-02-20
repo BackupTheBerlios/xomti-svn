42b4617b0dab7b77f2cfa13d5e39e30fc9fd79e2
fabrice_pardo <fabrice_pardo@597427b9-74ee-0310-9ab7-f5dcdbef3a4c>
2005-02-20 17:59:54 +0000

    Add original 0.74

---
+++ xotclIDE/tags/0.74/XotclIDEDB.tcl
@@ -0,0 +1,31 @@
+#!/bin/sh
+# \
+exec tclsh "$0" ${1+"$@"}
+
+# start File for use XotclIDE as stand alone application
+# please adapt the xotcl library path for your machine
+# you can use tk or tix for XotclIDE
+# (you can not use TixInspector without tix)
+
+set sname [info script]
+if {$sname==""} {
+    # Run interactive for develop purposes
+    set xotclidedir /home/artur/xotclIDE
+} else {
+    file lstat $sname stats
+    # follow sym links
+    if {$stats(type)=="link"} {
+	set sname [file readlink $sname]
+	if {[file pathtype $sname]=="relative"} {
+	    set sname [file join [file dirname [info script]] $sname]
+	}
+    }
+    set xotclidedir [file dirname $sname]
+    set xotclidedir [file normalize $xotclidedir]
+}
+
+source [file join $xotclidedir ideBase.tcl]
+source [file join $xotclidedir initDB.tcl]
+
+IDEStarter startIDEWithDB
+
