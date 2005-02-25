9d3732f9ed103828fa00d6a434deab7fd623c204
fabrice_pardo <fabrice_pardo@597427b9-74ee-0310-9ab7-f5dcdbef3a4c>
2005-02-25 16:13:15 +0000

    Make_pkgIndex.tcl invalidated, now I have to edit manually pkgIndex.tcl

--- make_pkgIndex.tcl
+++ make_pkgIndex.tcl
@@ -1,3 +1,4 @@
 set here [file dir [info script]]
-puts stderr "here = $here"
-pkg_mkIndex -verbose $here xomti*/tags/*/*.xotcl xomti*/trunk/*.xotcl
+puts stderr "not good" ; exit
+pkg_mkIndex -verbose $here xomti*/tags/*/*.xotcl xomti*/trunk/*.xotcl antirez_tclref/tags/*/*.xotcl antirez_tclref/tags/trunk/*.xotcl
+
