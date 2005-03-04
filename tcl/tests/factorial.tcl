89ac414c6ff5e50a79a60a7dab4c9ab110803460
fabrice_pardo <fabrice_pardo@597427b9-74ee-0310-9ab7-f5dcdbef3a4c>
2005-03-04 09:13:31 +0000

    small sync

--- tcl/tests/factorial.tcl
+++ tcl/tests/factorial.tcl
@@ -1,3 +1,6 @@
+memory validate off
+
+
 proc f1 {n} {
     set r $n
     incr n -1
@@ -16,48 +19,105 @@ proc f2 {n} {
     }
 }
 
-set x [proc {} {n} {
+set f3 [proc {} {f n} {
     if {$n == 2} {
 	return 2
     } else {
-	return [expr {[::$x [expr {$n-1}]] * $n}]
+	return [expr {[$f $f [expr {$n-1}]] * $n}]
     }
 }]
 
-proc add1 {a b} {
+time {f1 19} 10000
+time {f2 19} 10000
+time {$f3 $f3 19} 10000
+
+proc add {a b} {
     return [expr {$a + $b}]
 }
 
 proc t1 {} {
     set r 1.0
-    for {set i 1000} {$i > 0} {incr i -1} {
-	set r [add1 $r $r]
+    for {set i 100} {$i > 0} {incr i -1} {
+	set r [add $r $r]
     }
     return $r
 }
 
-proc t2 {} {
+proc t2 {f} {
     set r 1.0
-    for {set i 1000} {$i > 0} {incr i -1} {
-	set r [[proc {} {a b} {return [expr {$a + $b}]}] $r $r]
-    }
-    return $r
-}
-
-proc t3 {} {
-    set r 1.0
-    set f [proc {} {a b} {return [expr {$a + $b}]}]
     for {set i 100} {$i > 0} {incr i -1} {
 	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
-	set r [$f $r $r]
     }
     return $r
 }
+
+
+set aa [proc {} {a b} {
+    return [expr {$a + $b}]
+}]
+proc t3 {aa} {
+    set r 1.0
+    for {set i 100} {$i > 0} {incr i -1} {
+	set r [$aa $r $r]
+    }
+    return $r
+}
+
+time {t1} 100
+time {t2 add} 100
+time {t3 $aa} 100
+
+proc v {} {
+    return a
+}
+
+proc v1 {} {
+    set l ""
+    for {set i 1000} {$i > 0} {incr i -1} {
+	append l [v]
+    }
+    return $l
+}
+
+proc v2 {v} {
+    set l ""
+    for {set i 1000} {$i > 0} {incr i -1} {
+	append l [$v]
+    }
+    return $l
+}
+
+set vv [proc {} {} {return a}]
+
+proc v3 {vv} {
+    set l ""
+    for {set i 1000} {$i > 0} {incr i -1} {
+	append l [$vv]
+    }
+    return $l
+}
+
+
+time {v1} 100
+time {v2 v} 100
+time {v3 $vv} 100
+
+proc v1b {} {
+    return "D[v][v][v][v][v][v][v][v][v][v][v][v][v][v][v][v][v][v][v][v]"
+}
+proc v2b {v} {
+    return "D[$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v][$v]"
+}
+proc v3b {vv} {
+    return "D[$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv][$vv]"
+}
+
+time {v1b} 1
+time {v2b v} 1
+time {v3b $vv} 1
+
+time {v1b} 10000
+time {v2b v} 10000
+time {v3b $vv} 10000
+
+
